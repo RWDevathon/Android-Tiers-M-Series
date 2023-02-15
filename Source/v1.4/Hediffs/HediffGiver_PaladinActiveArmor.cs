828e6c555cf5203f94e21cb3eb86c49d4a2a90c2
Killathon <huelva21@gmail.com>
2023-02-14 20:10:44 -0800

    (0.2.0) Implemented first draft of M3, name tweaks.

---
+++ Source/v1.4/Hediffs/HediffGiver_PaladinActiveArmor.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+
+namespace ATReforged
+{
+    // The Paladin Active Armor hediff giver will give a incoming damage factor that scales with the amount of damage the unit has taken.
+    public class HediffGiver_PaladinActiveArmor : HediffGiver
+    {
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            float pawnHealthpct = pawn.health.summaryHealth.SummaryHealthPercent;
+
+            // A pawn is dead if they have less than 5% health by the SummaryHealthPercent method.
+            if (pawnHealthpct >= 0.05f)
+            {
+                Hediff activeArmorHediff = pawn.health.hediffSet.GetFirstHediffOfDef(hediff);
+
+                if (activeArmorHediff == null)
+                {
+                    activeArmorHediff = HediffMaker.MakeHediff(hediff, pawn);
+                    pawn.health.AddHediff(activeArmorHediff);
+                }
+
+                activeArmorHediff.Severity = pawnHealthpct;
+            }
+        }
+    }
+}
