76245ba80977a5a48ec57282d2e0058f863036b7
Killathon <huelva21@gmail.com>
2023-02-25 12:03:13 -0800

    (0.3.1) Minor language keys/harmony patch

---
+++ Source/v1.4/AndroidTiersMSeries.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace ATReforged
+{
+    public class ATReforged : Mod
+    {
+        public ATReforged(ModContentPack content) : base(content)
+        {
+            new Harmony("ATMSeries").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
